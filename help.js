02d6ad2a8fa48e999944b4389ebfcc937800116a
NICKZZIN <76818604+NICKZZIN@users.noreply.github.com>
2021-01-22 06:55:24 -0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,190 @@
+
+const help = (prefix) => { 
+	return `                 
+<══════════════════>
+      BOT BY 𝙽𝚒𝚌𝚔𝚣𝚣𝚒𝚗 - BOT
+ <══════════════════>
+__█████████  ● ᏴϴͲ●ᎷᎬΝႮ●         
+__█▄█████▄█   Author:𝙽𝚒𝚌𝚔𝚣𝚣𝚒𝚗
+__█▼▼▼▼▼█ 
+_██BOT V3.0██▌ 
+__█▲▲▲▲▲█ 
+__█████████ 
+____██_____██
+┌────────3.0────────┐
+└────────3.0────────┘
+╔══════════════════
+║ 𝑪𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒍𝒊𝒔𝒕𝒂𝒅𝒐𝒔
+╠══════════════════
+║   𝑬𝒙𝒄𝒍𝒖𝒔𝒊𝒗𝒐𝒔 𝒑𝒂𝒓𝒂 𝒂𝒅𝒊𝒎𝒊𝒏𝒊𝒔𝒕𝒓𝒂𝒅𝒐𝒓𝒆𝒔
+║╭─────────────────
+║├>☻︎ /welcome [0/1]
+║├>☻︎ /bv [0/1]
+║├>☻︎ /bemvindo [0/1]
+║├>☻︎ /boasvindas [0/1]
+║├────> desativar/ativa bv
+║├─────────────────
+║├>☻︎ /add 55xxx
+║├>☻︎ /adicionar 55xxx
+║├────> add menbro
+║├─────────────────
+║├>☻︎ /kick @membro
+║├>☻︎ /ban @membro
+║├>☻︎ /remover @membro
+║├────> remover membro
+║├─────────────────
+║├>☻︎ /promote @membro
+║├>☻︎ /promover @membro
+║├────> promover a ADM
+║├─────────────────
+║├>☻︎ /demote @adm
+║├>☻︎ /promover @adm
+║├────> rebaixar a membro
+║├─────────────────
+║├>☻︎ /grup abrir/fechar
+║├>☻︎ /group abrir/fechar
+║├>☻︎ /grupo abrir/fechar
+║├────> bem específico ;-;
+║├─────────────────
+║├>☻︎ /setname seu txt
+║├>☻︎ /mudarnome seu txt
+║├────> troca nome do grupo
+║├─────────────────
+║├>☻︎ /setdesc seu txt
+║├>☻︎ /mudardesc seu txt
+║├────> troca descrição
+║├─────────────────
+║├>☻︎ /mudarfoto
+║├────> mudar foto do grupo
+║├─────────────────
+║├>☻︎ /marcar
+║├────> marcação
+║├─────────────────
+║├>☻︎ /marcar2
+║├────> marcação 2
+║├─────────────────
+║├>☻︎ /marcar3
+║├────> marcação
+║├─────────────────
+║├> 𝑫𝒐𝒘𝒏𝒍𝒐𝒂𝒅/𝑳𝒊𝒏𝒌
+║├─────────────────
+║├>シ︎ /tiktokstalk Nick da conta
+║├────> stalk de tiktok
+║├─────────────────
+║├>シ︎ /fototiktok
+║├────> foto do tiktok
+║├─────────────────
+║├>シ︎ /ytmp4 link
+║├>シ︎ /ytvideo link
+║├>シ︎ /ytbuscar link
+║├>シ︎ /ytbaixa link
+║├────> baixa vídeo do YouTube
+║├─────────────────
+║├>𝑴𝒊𝒅𝒊𝒂/𝑶𝒖𝒕𝒓𝒐𝒔
+║├─────────────────
+║├>❦︎ /sticker
+║├>❦︎ /stiker
+║├>❦︎ /fig
+║├>❦︎ /figurinha
+║├────> fazer fig cm foto/gif
+║├─────────────────
+║├>❦︎ /tts seu txt
+║├>❦︎ /gtts seu txt
+║├>❦︎ /audio seu txt
+║├────> converter txt em áudio
+║├─────────────────
+║├>❦︎ /toimg marca fig
+║├>❦︎ /converter marca fig
+║├────> converter fig em foto
+║├─────────────────
+║├>❦︎ /txtdafoto
+║├>❦︎ /ocr
+║├────> pega txt de foto
+║├─────────────────
+║├>❦︎ /wait marca foto
+║├>❦︎ /pesquisar marca foto
+║├────> pesquisa por anime de ft
+║├─────────────────
+║├>❦︎ /listaadms
+║├>❦︎ /listadmins
+║├>❦︎ /adms
+║├────> lista dos adms
+║├─────────────────
+║├>❦︎ /cachorro
+║├>❦︎ /auau
+║├>❦︎ /dog
+║├────> dog fofo aleatório
+║├─────────────────
+║├>❦︎ /pokemon
+║├────> pokemon aleatório
+║├─────────────────
+║├>❦︎ /logo3d seu txt
+║├────> logo cm seu texto
+║├─────────────────
+║├>❦︎ /logoph seu|txt
+║├────> logo cm seu texto
+║├─────────────────
+║├>❦︎ /info
+║├────> informações do bot
+║├─────────────────
+║├>❦︎ /listabloq
+║├────> números bloqueados
+║├─────────────────
+║├>❦︎ /lofi
+║├────> foto única de lofi
+║├─────────────────
+║├>❦︎ /map 0000-0000 
+║├>❦︎ /mapa 0000-0000 
+║├────> pega foto do local
+║├─────────────────
+║├>𝑪𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒅𝒐 𝒄𝒓𝒊𝒂𝒅𝒐𝒓
+║├─────────────────
+║├>☀︎︎ /hidetag
+║├────> esconde tag
+║├─────────────────
+║├>☀︎︎ /clearall
+║├>☀︎︎ /limpar
+║├────> limpa zap do bot
+║├─────────────────
+║├>☀︎︎ /bloquear @membro
+║├────> autobloq
+║├─────────────────
+║├>☀︎︎ /desbloquear
+║├────> autodesbloq
+║├─────────────────
+║├>☀︎︎ /kitar
+║├────> bot sai fo grupo
+║├─────────────────
+║├>☀︎︎ /bc
+║├────> [?]
+║├─────────────────
+║├>☀︎︎ /clonar
+║├>☀︎︎ /clone
+║├────> clonar tag
+║├─────────────────
+║├>  𝑺𝒖𝒑𝒐𝒓𝒕𝒆
+║├─────────────────
+║├>∆ /bug
+║├>∆ /reportar
+║├>∆ /reportarbug
+║├────> reportar bug ao criador
+║├─────────────────
+║├>∆ /sigla nova sigla
+║├>∆ /setprefix
+║├────> troca sigla de comandos
+║└─────────────────
+╚══════════════════
+─>                ╲    ╱    💠𝙽𝚒𝚌𝚔𝚣𝚣𝚒𝚗💠
+──>      ╱▔▔▔▔▔╲       
+──>    ┃┈▇┈┈▇┈┃      
+─>╭╮┣━━━━━━┫╭╮    
+─>┃┃┃┈┈┈┈┈┈┃┃┃    
+─>╰╯┃┈┈┈┈┈┈┃╰╯
+─>        ╰┓┏━━┓┏╯
+─>            ╰╯        ╰╯
+┌─────────3.0───────┐
+      𝙽𝚒𝚌𝚔𝚣𝚣𝚒𝚗 𝚖𝚘𝚍𝚜  
+└─────────3.0───────┘
+`
+}
+exports.help = help
