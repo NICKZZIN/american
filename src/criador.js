6edf3d8424674f899e0bf9eeb6418da5a033664d
NICKZZIN <76818604+NICKZZIN@users.noreply.github.com>
2021-01-22 06:57:00 -0300

    Add files via upload

---
+++ src/criador.js
@@ -0,0 +1,14 @@
+const criador = (prefix) => { 
+	return `                 
+	
+┌────────────────┐
+    BOT: 𝙱𝚘𝚝 𝚍𝚘 𝙽𝚒𝚌𝚔𝚣𝚣𝚒𝚗
+    instagram: https://www.instagram.com/n1ckzz1n/
+    Número do criador: wa.me/5512997277680
+    youtube: https://youtu.be/D58GrbAC1fA
+    Dm de chefe: nivel 87
+└────────────────┘
+	
+	`
+}
+exports.criador = criador
