a807793896d69eb6d840e291951d41900edc9392
NICKZZIN <76818604+NICKZZIN@users.noreply.github.com>
2021-01-25 00:18:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ client.on('group-participants-update', async (anu) => {
 				case 'menu':
 					client.sendMessage(from, help(prefix), text)
 					break
-               case 'CRICRI: 
+               case 'Cricrir: 
 				case 'cricri':
 					client.sendMessage(from, criador(prefix), text)
 					break
