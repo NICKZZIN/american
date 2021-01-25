99969a40da8a85ba0f92c683f15d19c0aa93ffc5
NICKZZIN <76818604+NICKZZIN@users.noreply.github.com>
2021-01-25 00:07:52 -0300

    Add files via upload

--- criador.js
+++ criador.js
@@ -1,14 +1,15 @@
+
 const criador = (prefix) => { 
 	return `                 
-	
-┌────────────────┐
-    BOT: 𝙱𝚘𝚝 𝚍𝚘 𝙽𝚒𝚌𝚔𝚣𝚣𝚒𝚗
+<══════════════════>
+   Obrigado Por Usar O Bot Do Nickzzin
+    INFBOT: 𝙱𝚘𝚝 𝚍𝚘 𝙽𝚒𝚌𝚔𝚣𝚣𝚒𝚗
     instagram: https://www.instagram.com/n1ckzz1n/
-    Número do criador: wa.me/5512997277680
-    youtube: https://youtu.be/D58GrbAC1fA
-    Dm de chefe: nivel 87
-└────────────────┘
-	
-	`
+    nomer: wa.me/5512997277680
+    youtube: https://youtu.be/D58GrbAC1fA'
+    Meu Whatsapp: Em breve...
+    Sérião Amo Oce Rapaz/Moça Continue Com Sua Inteligência Por fv.
+<══════════════════>
+`
 }
 exports.criador = criador
